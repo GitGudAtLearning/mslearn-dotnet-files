09f5978472dd2b608b1f1172b9079baebb2b220c
GitGudAtLearning <121337342+GitGudAtLearning@users.noreply.github.com>
2025-03-14 15:56:59 +0100

    Add initial project files and implement sales file retrieval functionality

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿var salesFiles = FindFiles("stores");
+
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    var salesFiles = foundFiles.Where(file => file.EndsWith("sales.json")).ToList();
+
+    return salesFiles;
+}
+
+// var salesFiles = FindFiles("stores");
+    
+// foreach (var file in salesFiles)
+// {
+//     Console.WriteLine(file);
+// }
+
+// IEnumerable<string> FindFiles(string folderName)
+// {
+//     List<string> salesFiles = new List<string>();
+
+//     var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+//     foreach (var file in foundFiles)
+//     {
+//         // The file name will contain the full path, so only check the end of it
+//         if (file.EndsWith("sales.json"))
+//         {
+//             salesFiles.Add(file);
+//         }
+//     }
+
+//     return salesFiles;
+// }
